2428e5d15d25269961d5d6e30d525faba84c1a01
Fran Linde Blazquez <franlindebl@gmail.com>
2023-04-28 09:13:43 +0200

    Added node-s6 code

---
+++ models/Brand.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const brandSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    creationYear: {
+      type: Number,
+      required: false,
+    },
+    country: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Brand = mongoose.model("Brand", brandSchema);
+module.exports = { Brand };
---
+++ models/Car.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Creamos el schema del coche
+const carSchema = new Schema(
+  {
+    brand: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+      required: false,
+    },
+    model: {
+      type: String,
+      required: true,
+    },
+    plate: {
+      type: String,
+      required: false,
+    },
+    power: {
+      type: Number,
+      required: false,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Car = mongoose.model("Car", carSchema);
+module.exports = { Car };
